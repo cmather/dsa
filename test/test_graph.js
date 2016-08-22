39d0bde93f7a99f8c94e38f7e67b50e329153dcd
Chris Mather <chris@eventedmind.com>
2016-08-22 12:35:26 -0700

    refactored graph class

--- test/test_graph.js
+++ test/test_graph.js
@@ -1,112 +1,129 @@
 import assert from 'assert';
-import Graph, {Vertex, Edge} from '../lib/graph';
+import Graph, {Node, Edge} from '../lib/graph';
 
-describe("Vertex", () => {
+describe("Node", () => {
   it ("should construct", () => {
-    let v = new Vertex('a');
+    let g = new Graph;
+    let v = new Node(g, 'a');
   });
 
-  it ("should allow adding edges of raw values", () => {
-    let v = new Vertex('a');
-    v.addEdge('b');
-    v.addEdge('c');
-    assert(v.degree == 2);
-    assert(v.edges[0].to.value == 'b');
-    assert(v.edges[1].to.value == 'c');
-  });
-
-  it ("should allow adding edge of type Vertex", () => {
-    let a = new Vertex('a');
-    let b = new Vertex('b');
+  it ("should provide iteration over edges", () => {
+    let g = new Graph;
+    let a = new Node(g, 'a');
+    let b = new Node(g, 'b');
     a.addEdge(b);
-    assert(a.degree == 1);
-    assert(a.edges[0].to == b);
-  });
-
-  it ("should allow adding edges of type Edge", () => {
-    let a = new Vertex('a');
-    let b = new Vertex('b');
-    let edge = new Edge(a, b);
-    a.addEdge(edge);
-    assert(a.degree == 1);
-    assert(a.edges[0].to == b);
-  });
-
-  it ("should allow an explicitly set null key as an option", () => {
-    let v = new Vertex(null, {key: null});
-    assert(v.key == null, `v.key is ${v.key} instead of null.`);
+    let results = [];
+    for (let edge of a) {
+      results.push(edge);
+    }
+    assert.equal(results.length, 1);
+    assert.equal(results[0].from, a);
+    assert.equal(results[0].to, b);
   });
 });
 
-describe("Graph", () => {
+describe ("Graph", () => {
   it ("should construct", () => {
     let g = new Graph;
+  });
+
+  it ("should allow adding nodes", () => {
+    let g = new Graph();
+    g.add('a');
+    assert.equal(g.length, 1);
+    assert.equal(g.size, 1);
+  });
+
+  it ("should allow iteration of nodes with for of loop", () => {
+    let g = new Graph;
+    let a = g.add('a');
+    let b = g.add('b');
+    let results = [];
+    let expected = ['a', 'b'];
+    assert.equal(g.size, 2, 'graph size should be 2');
+    for (let node of g) results.push(node);
+    assert.equal(results.length, 2, 'should have 2 results');
+    results.forEach((node, idx) => assert.equal(node.value, expected[idx]));
+  });
+
+  it ("should allow iteration of nodes with forEach", () => {
+    let g = new Graph;
+    let a = g.add('a');
+    let b = g.add('b');
+    let results = [];
+    let expected = [a, b];
+    g.forEach((node, idx) => assert.equal(node, expected[idx]));
+  });
+
+  it ("should allow iteration of nodes with generator", () => {
+    let g = new Graph;
+    let a = g.add('a');
+    let b = g.add('b');
+    let results = [];
+    let idx = 0;
+    let iter = g.nodes();
+    let cur = iter.next();
+
+    let expected = [a, b];
+    while (!cur.done) {
+      assert.equal(cur.value, expected[idx++]);
+      cur = iter.next();
+    }
   });
 
   it ("should allow adding undirected edges", () => {
     let g = new Graph;
-    g.addEdge('a', 'b');
-    assert(g.length == 2);
-    assert(g.vertices[0].value == 'a');
-    assert(g.vertices[0].degree == 1);
-    assert(g.vertices[0].edges[0].to.value == 'b');
-    assert(g.vertices[1].value == 'b');
-    assert(g.vertices[1].degree == 1);
-    assert(g.vertices[1].edges[0].to.value == 'a');
+    let a = g.add('a');
+    let b = g.add('b');
+    let edge = g.addEdge(a, b);
+
+    let expected = [{
+      value: 'a',
+      degree: 1,
+      edges: [{from: a, to: b}]
+    }, {
+      value: 'b',
+      degree: 1,
+      edges: [{from: b, to: a}]
+    }];
+
+    assert.equal(g.size, 2);
+
+    g.forEach((node, i) => {
+      assert.equal(node.value, expected[i].value);
+      assert.equal(node.degree, expected[i].edges.length);
+      node.forEach((edge, j) => {
+        assert.equal(edge.from, expected[i].edges[j].from);
+        assert.equal(edge.to, expected[i].edges[j].to);
+      });
+    });
   });
 
   it ("should allow adding directed edges", () => {
     let g = new Graph({directed: true});
-    g.addEdge('a', 'b');
-    assert(g.length == 1);
-    assert(g.vertices[0].value == 'a');
-    assert(g.vertices[0].degree == 1);
-    assert(g.vertices[0].edges[0].to.value == 'b');
-  });
+    let a = g.add('a');
+    let b = g.add('b');
+    let edge = g.addEdge(a, b);
 
-  it ("should allow adding vertices", () => {
-    let g = new Graph();
-    g.addVertex('a');
-    assert(g.length == 1);
-    assert(g.vertices[0].value == 'a');
-  });
+    let expected = [{
+      value: 'a',
+      degree: 1,
+      edges: [{from: a, to: b}]
+    }, {
+      value: 'b',
+      degree: 1,
+      edges: []
+    }];
 
-  it ("should allow iterating root vertices", () => {
-    let g = new Graph({directed: true});
-    g.addVertex('a');
-    g.addVertex('b');
-    let vertex = g.addVertex('c');
-    vertex.addEdge('d');
+    assert.equal(g.size, 2);
 
-    let results = [];
-    for (var v of g) {
-      results.push(v && v.value);
-    }
-
-    assert.equal(results.length, 3);
-    assert.equal(g.length, 3);
-    assert.equal(results[0], 'a');
-    assert.equal(results[1], 'b');
-    assert.equal(results[2], 'c');
-  });
-
-  it ("should provide allVertices to iterate over all vertices in graph", () => {
-    let g = new Graph({directed: true});
-    g.addVertex('a');
-    g.addVertex('b');
-    let vertex = g.addVertex('c');
-    vertex.addEdge('d');
-
-    let results = [];
-    for (var v of g.allVertices()) {
-      results.push(v && v.value);
-    }
-
-    assert.equal(results.length, 4);
-    assert.equal(g.length, 3);
-    assert.equal(results[0], 'a');
-    assert.equal(results[1], 'b');
-    assert.equal(results[2], 'c');
-    assert.equal(results[3], 'd');
+    g.forEach((node, i) => {
+      assert.equal(node.value, expected[i].value);
+      assert.equal(node.degree, expected[i].edges.length);
+      node.forEach((edge, j) => {
+        assert.equal(edge.from, expected[i].edges[j].from);
+        assert.equal(edge.to, expected[i].edges[j].to);
+      });
+    });
   });
 });
