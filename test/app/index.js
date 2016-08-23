05b4428a758ad77e7047b9c457eccd9921ff494e
Chris Mather <chris@eventedmind.com>
2016-08-23 10:41:05 -0700

    moved problems into problems dir

--- test/app/index.js
+++ test/app/index.js
@@ -1,22 +1,27 @@
 import Queue from '../../lib/queue';
 import Stack from '../../lib/stack';
 import Graph from '../../lib/graph';
+import HashTable from '../../lib/hash_table';
 import {bfs, dfs} from '../../lib/graph_search';
 import MinHeap from '../../lib/min_heap';
 import MaxHeap from '../../lib/max_heap';
 import MinPriorityQueue from '../../lib/min_priority_queue';
+import BinaryTree, {BinaryNode} from '../../lib/binary_tree';
 import BinarySearchTree from '../../lib/binary_search_tree';
-import RedBlackTree, {NullNode} from '../../lib/red_black_tree';
+import RedBlackTree, {RBNullNode} from '../../lib/red_black_tree';
 import Dijkstra, {shortestPath} from '../../lib/dijkstra';
-import justifyText, {JustifyText} from '../../lib/justify_text';
-import phoneWordCombos from '../../lib/phone_word_combos';
+import {selectionSort, bubbleSort, quickSort} from '../../lib/sort';
+import compare from '../../lib/compare';
 
 window.dsa = {
+  compare: compare,
   Queue: Queue,
   Stack: Stack,
   Graph: Graph,
+  HashTable: HashTable,
   bfs: bfs,
   dfs: dfs,
+  BinaryTree: BinaryTree,
   BinarySearchTree: BinarySearchTree,
   RedBlackTree: RedBlackTree,
   MinHeap: MinHeap,
@@ -24,21 +29,7 @@ window.dsa = {
   MinPriorityQueue: MinPriorityQueue,
   Dijkstra: Dijkstra,
   shortestPath: shortestPath,
-  JustifyText: JustifyText,
-  justifyText: justifyText,
-  phoneWordCombos: phoneWordCombos
+  selectionSort: selectionSort,
+  bubbleSort: bubbleSort,
+  quickSort: quickSort
 };
-
-let words = phoneWordCombos(2345678);
-let index = {};
-
-words.forEach((word) => {
-  let key = word.join('');
-
-  if (index[key])
-    throw new Error(`duplicate ${key}`);
-  else
-    index[key] = true;
-});
-
-console.log(words);
